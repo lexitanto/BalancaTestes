335c06b8723115d0c39600cd14a951b25f6de488
kauan resende <kauan26fr@outlook.com>
2025-03-26 09:57:11 -0300

    enviar dados servidor

--- bin/app/main.py
+++ bin/app/main.py
@@ -8,17 +8,17 @@ from tracking_module import tracking
 
 def main():
     # GPS = gps()
-    BALANCA = balanca()
+    # BALANCA = balanca()
     LED = led()
     STATUS = tracking()
 
     # gps_thread = threading.Thread(target=GPS.run, daemon=True)
-    balanca_thread = threading.Thread(target=BALANCA.run, daemon=True)
+    # balanca_thread = threading.Thread(target=BALANCA.run, daemon=True)
     led_thread = threading.Thread(target=LED.monitor, daemon=True)
     status_thread = threading.Thread(target=STATUS.enviar_dados, daemon=True)
 
     # gps_thread.start()
-    balanca_thread.start()
+    # balanca_thread.start()
     led_thread.start()
     status_thread.start()
 
