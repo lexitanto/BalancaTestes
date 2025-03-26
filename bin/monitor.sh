8b7a3918fcf1e24b5fba128fb35de7c53df20874
kauan resende <kauan26fr@outlook.com>
2025-03-26 16:04:26 -0300

    26.03 -> combining cronjob and systemd to monitor.sh and repoupdate

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -3,12 +3,9 @@
 . config.sh
 LOG_FILE="/tmp/monitor.log"
 
-while true; do
-    if ! pgrep -f "python3 $REPO_PATH/bin/app/main.py" > /dev/null; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] Starting app..." | tee -a "$LOG_FILE"
+if ! pgrep -f "python3 $REPO_PATH/bin/app/main.py" > /dev/null; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] Starting app..." | tee -a "$LOG_FILE"
         $CMD_INICIAR &
-    else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] App is running." | tee -a "$LOG_FILE"
-    fi
-    sleep "$ONEHOUR"
-done
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] App is running." | tee -a "$LOG_FILE"
+fi
