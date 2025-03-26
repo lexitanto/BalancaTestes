00367d6d57b96d75bf2d1061ff61d19a886b3297
kauan resende <kauan26fr@outlook.com>
2025-03-26 10:02:58 -0300

    enviar dados servidor

--- bin/app/tracking_module.py
+++ bin/app/tracking_module.py
@@ -1,7 +1,7 @@
 import os
 import time
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 URL_SERVER = "https://7948-170-80-64-72.ngrok-free.app/IoT/Balanca/status"
@@ -13,7 +13,7 @@ class tracking():
         if os.path.exists(DEVICE_PATH):
             with open(DEVICE_PATH, 'r') as f:
                 NUMERO_SERIAL = f.read().strip()
-        dados = f'{NUMERO_SERIAL};Dispositivo online;{datetime.now()}'
+        dados = f'{NUMERO_SERIAL};Dispositivo online;{datetime.now(timezone.utc)}'
         texto_byte = bytes(dados, 'utf-8')
         headers = {"Content-Type": "application/octet-stream"}
         try:
