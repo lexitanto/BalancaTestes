298b3737d44642fc48e6554efc97e441df0e114e
kauan resende <kauan26fr@outlook.com>
2025-03-27 09:33:31 -0300

    back with the loop in monitor.sh cuz cronjob doesnt work

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -3,9 +3,16 @@
 REPO_PATH="/opt/BalancaTestes"
 . "$REPO_PATH/bin/config.sh"
 
+while true;do
+
 if ! pgrep -f "python3 $REPO_PATH/bin/app/main.py" > /dev/null; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] Starting app..." | tee -a "$LOG_FILE"
         nohup $CMD_INICIAR > /dev/null 2>&1 &
 else
     echo "$(date '+%Y-%m-%d %H:%M:%S') - [Monitor] App is running." | tee -a "$LOG_FILE"
 fi
+
+sleep "$ONEHOUR"
+
+done
+
