5de5197a5863223baf8ddedf5ecfe734d1826c26
kauan resende <kauan26fr@outlook.com>
2025-03-27 10:44:23 -0300

    network-online.target é uma bosta

--- bin/repoupdate.sh
+++ bin/repoupdate.sh
@@ -3,6 +3,11 @@
 REPO_PATH="/opt/BalancaTestes"
 . "$REPO_PATH/bin/config.sh"
 
+while ! ping -c 1 8.8.8.8 &> /dev/null; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - [Update] Aguardando conexão com a internet..."
+    sleep 5
+done
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - [Update] Atualizando repositório..." | tee -a "$LOG_FILE"
 
 sudo git -C $REPO_PATH fetch origin
